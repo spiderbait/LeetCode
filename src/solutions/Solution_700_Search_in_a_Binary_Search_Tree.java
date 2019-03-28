de27e1c276e4d7eeff1db7db89224c9317926885
litianzhuo <litianzhuo@bilibili.local>
2019-03-28 11:17:42 +0800

    modify solution of 700

--- src/solutions/Solution_700_Search_in_a_Binary_Search_Tree.java
+++ src/solutions/Solution_700_Search_in_a_Binary_Search_Tree.java
@@ -11,18 +11,14 @@ class TreeNode {
 
 public class Solution_700_Search_in_a_Binary_Search_Tree {
 	
+	private TreeNode targetNode = null;
+	
 	public TreeNode searchBST(TreeNode root, int val) {
 		if (root == null) {
-			
+			return null;
 		}
-		if ()
-		System.out.println(root.val);
-		if (root.left != null) {
-			traverse(root.left);
-		}
-		if (root.right != null) {
-			traverse(root.right);
-		}
+		getTargetNode(root, val);
+		return this.targetNode;
     }
 	
 	public void traverse(TreeNode root) {
@@ -38,12 +34,27 @@ public class Solution_700_Search_in_a_Binary_Search_Tree {
 		}
 	}
 	
+	public void getTargetNode(TreeNode root, int val) {
+		if (root.val == val) {
+			this.targetNode = root;
+		}
+		if (root.left != null) {
+			getTargetNode(root.left, val);
+		}
+		if (root.right != null) {
+			getTargetNode(root.right, val);
+		}
+	}
+	
+	public TreeNode getTargetNode() {
+		return this.targetNode;
+	}
+	
 	public static void main(String[] args) {
 		TreeNode root = new TreeNode(4, new TreeNode(2, new TreeNode(1, null, null), new TreeNode(3, null, null)), new TreeNode(7, null, null));
 		
 		Solution_700_Search_in_a_Binary_Search_Tree obj = new Solution_700_Search_in_a_Binary_Search_Tree();
-//		System.out.println(obj.searchBST(root, 2));
-		obj.traverse(root);
+		obj.traverse(obj.searchBST(root, 2));	
 	}
 
 }
